fa20fed287e53b089b454f93c382a48a818f2a6f
shailja111 <122468623+shailja111@users.noreply.github.com>
2023-02-21 23:43:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 //your code here
-var btn = document.getElementById("btn");
-var numPtag = document.getElementById("num");
-function generateRandomNumber()
-	{
-		var no = Math.floor(Math.random() * 20);
-		numPtag.textContent = no;
-		
-		
+
+var btn=document.getElementById("btn");
+var numPtag=document.getElementById("num");
+var res=document.getElementById("respond");
+var inputValue=document.getElementById("guess").value;
+
+function RandomNumGenerator() {
+  randomNum=Math.floor(Math.random() *41) -20;
+  document.getElementById("num").innerText=randomNum;
+  guessNum();
+  print();
+}
+
+function guessNum(){
+  input=Number(document.getElementById("guess").value);
+}
+function print(){
+  if(Math.abs(randomNum-input)<=5){
+    document.getElementById("respond").innerHTML="Hot";
+  }
+  else{
+    document.getElementById("respond").innerHTML="Cold"
+  }
+}
+btn.addEventListener('click',RandomNumGenerator);
 	}
 btn.addEventListener('click', generateRandomNumber);

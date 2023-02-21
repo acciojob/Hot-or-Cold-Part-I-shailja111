fccc944d8410b8bd18bfafe38edb6828ee85fc76
shailja111 <122468623+shailja111@users.noreply.github.com>
2023-02-21 23:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+var btn = document.getElementById("btn");
+var numPtag = document.getElementById("num");
+function generateRandomNumber()
+	{
+		var no = Math.floor(Math.random() * 20);
+		numPtag.textContent = no;
+		
+		
+	}
+btn.addEventListener('click', generateRandomNumber);
